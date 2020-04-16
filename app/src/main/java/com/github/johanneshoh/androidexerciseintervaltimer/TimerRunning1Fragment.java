a060aec28884184fd008f1b1e85ede34a8865efa
Johannes Hohenbichler <johanneshoh@googlemail.com>
2020-04-16 14:27:19 +0200

    fragments step 3 - portrait layout complete

---
+++ app/src/main/java/com/github/johanneshoh/androidexerciseintervaltimer/TimerRunning1Fragment.java
@@ -0,0 +1,64 @@
+package com.github.johanneshoh.androidexerciseintervaltimer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TimerRunning1Fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TimerRunning1Fragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public TimerRunning1Fragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment TimerRunningFragment1.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static TimerRunning1Fragment newInstance(String param1, String param2) {
+        TimerRunning1Fragment fragment = new TimerRunning1Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_timer_running1, container, false);
+    }
+}
