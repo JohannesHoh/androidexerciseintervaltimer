ad7b1274238fe0720c30cebedc243a1d7d8de310
Johannes Hohenbichler <johanneshoh@googlemail.com>
2020-04-16 14:37:04 +0200

    fragements cleanup

--- app/src/main/java/com/github/johanneshoh/androidexerciseintervaltimer/MainSetTimes1Fragment.java
+++ app/src/main/java/com/github/johanneshoh/androidexerciseintervaltimer/MainSetTimes1Fragment.java
@@ -9,50 +9,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MainSetTimes1Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MainSetTimes1Fragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public MainSetTimes1Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainSetTimes.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MainSetTimes1Fragment newInstance(String param1, String param2) {
         MainSetTimes1Fragment fragment = new MainSetTimes1Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
